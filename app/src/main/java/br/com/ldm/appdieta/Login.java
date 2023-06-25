804eed9d781a2c849f953f22e6152408274a1d46
Kevyn Hotts <kevynh36@gmail.com>
2023-06-25 19:41:39 -0400

    activity login and cadastro - ok

--- app/src/main/java/br/com/ldm/appdieta/Login.java
+++ app/src/main/java/br/com/ldm/appdieta/Login.java
@@ -2,9 +2,16 @@ package br.com.ldm.appdieta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,18 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Login.this, Cadrasto.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
