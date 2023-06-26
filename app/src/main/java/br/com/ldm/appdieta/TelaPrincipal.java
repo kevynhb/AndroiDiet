651507f7947ee56d38919ca4cc7f54856d20ca99
Kevyn Hotts <kevynh36@gmail.com>
2023-06-26 02:17:03 -0400

    telas finish

--- app/src/main/java/br/com/ldm/appdieta/TelaPrincipal.java
+++ app/src/main/java/br/com/ldm/appdieta/TelaPrincipal.java
@@ -1,7 +1,6 @@
 package br.com.ldm.appdieta;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class TelaPrincipal extends AppCompatActivity {
@@ -10,5 +9,7 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        getSupportActionBar().hide();
     }
 }
